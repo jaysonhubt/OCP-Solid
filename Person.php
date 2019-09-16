8e365b5bf1dd66d3b72bc332cd3f9c78d627124f
jaysonhubt <40816812+jaysonhubt@users.noreply.github.com>
2019-09-17 00:58:27 +0700

    Update Person.php

--- Person.php
+++ Person.php
@@ -2,9 +2,7 @@
 
 abtract class Person
 {
-    abstract protected function breathe();
+    abstract protected function greating($name);
     
     abstract protected function eat();
-    
-    abstract protected function drink();
 }
